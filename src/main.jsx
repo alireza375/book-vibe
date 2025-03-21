093acaa469e8a6d47e8dcdbeefdcb58e0effe13b
alireza375 <alirezaraju8@gmail.com>
2025-03-21 16:54:38 +0600

    component setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Dashboard from './components/Dashboard/Dashboard';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h2>Hello World!</h2>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>
+      }
+    ]
   },
 ]);
 
